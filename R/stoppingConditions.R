d9e4035d0b6c47c44582f2751d5ea3601b7b9cbc
MarcusCramer91 <marcus_cramer@t-online.de>
2015-12-22 13:49:26 +0100

    Update stoppingConditions.R

--- R/stoppingConditions.R
+++ R/stoppingConditions.R
@@ -33,7 +33,7 @@ stopOnIndefCovMat = function() {
     message = "Covariance matrix is not numerically positive definite.",
     stop.fun = function(envir = parent.frame()) {
       e.values = envir$e$values
-      return(any(e.values <= sqrt(.Machine$double.eps) * abs(max(e.values))))
+      return(any(e.values <= sqrt(.Machine$double.eps) * abs(e.values[1L])))
     }
   ))
 }
@@ -141,8 +141,8 @@ stopOnMaxEvals = function(max.evals) {
 #'   Tolerance value.
 #' @return [\code{cma_stopping_condition}]
 #' @export
-#FIXME: default value is 10^(-12) * sigma. Here we have no access to the sigma value.
-stopOnTolX = function(tol = 10^(-12)) {
+#FIXME: default value is 1e-12 * sigma. Here we have no access to the sigma value.
+stopOnTolX = function(tol = 1e-12) {
   assertInt(tol, na.ok = FALSE)
   force(tol)
   return(makeStoppingCondition(
@@ -170,8 +170,8 @@ stopOnNoEffectAxis = function() {
       ii = (envir$iter %% envir$n) + 1L
       ui = envir$e$vectors[, ii]
       lambdai = sqrt(envir$e$values[ii])
-      m.old = envir$m.old
-      return(sum((m.old - (m.old + 0.1 * envir$sigma * lambdai * ui))^2) < .Machine$double.eps)
+      m = envir$m
+      return(sum((m - (m + 0.1 * envir$sigma * lambdai * ui))^2) < .Machine$double.eps)
     }
   ))
 }
@@ -189,8 +189,8 @@ stopOnNoEffectCoord = function() {
     name = "noEffectCoord",
     message = "Addition of 0.2 times sigma in any coordinate does not change mean value.",
     stop.fun = function(envir = parent.frame()) {
-      m.old = envir$m.old
-      return(sum((m.old - (m.old + 0.2 * envir$sigma))^2) < .Machine$double.eps)
+      m = envir$m
+      return(sum((m - (m + 0.2 * envir$sigma))^2) < .Machine$double.eps)
     }
   ))
 }
@@ -217,72 +217,3 @@ stopOnCondCov = function(tol = 1e14) {
     }
   ))
 }
-
-
-#===============================================================================
-#=============================Online Convergence Detection======================
-#===============================================================================
-#' @title Stopping condition: Online Convergence Detection.
-#'
-#' @description Stop if OCD....
-#'
-#' @return [\code{cma_stopping_condition}]
-#' @family stopping conditions
-#' @export
-stopOnOCD = function(varLimit, nPreGen, maxGen)
-{
-  # Check if varLimit is a single numeric
-  assertNumber(varLimit, na.ok = FALSE)
-  # Check if nPreGen is a single integerish value
-  assertInt(nPreGen, na.ok = FALSE)
-  # initialize significane level alpha with default value 0.05
-  alpha = 0.05
-  # Check if maxGen is a single integerish value
-  assertInt(maxGen, na.ok = FALSE)
-  # return stopping condition being compatible with cma-es implementation by Jakob Bossek
-  return(makeStoppingCondition(
-    name = "Online Convergence Detection",
-    message = sprintf("OCD successfully: Variance limit %f", varLimit),
-    stop.fun = function(envir = parent.frame()) {
-      # Check if the number of iterations exceeds the user-defined number of maxGen. If TRUE, stop cma-es
-      if(envir$iter >= maxGen){
-        return(envir$iter >= maxGen)
-      }
-      # Check if number of iterations is greater than user-defined nPreGen
-      if(envir$iter > nPreGen){
-        # PI_all is a vector with one entry for each generation. 
-        # PI_all stores the difference between the performance indicator value of the preceding and the current generation.
-        # Here: In single objective optimization, the performance indicator of interest is the best fitness value of each generation.
-        PI_all = rbind(envir$generation.fitness[[1]], cbind(unlist(envir$generation.fitness))) - rbind(cbind(unlist(envir$generation.fitness)), envir$best.fitness)
-        # PI_current_gen is a subset of PI_all which stores the last nPreGen indicator values with respect to the current generation i.
-        PI_current_gen = PI_all[(envir$iter-nPreGen):envir$iter]
-        if((envir$iter - nPreGen) > 1){
-          # PI_preceding_gen is a subset of PI_all which stores the last nPreGen indicator values with respect to the last generation i-1.
-          PI_preceding_gen =  PI_all[(envir$iter - (nPreGen+1)):(envir$iter - 1)]
-        }else{
-          PI_preceding_gen = PI_current_gen
-        }
-        # perform chi2 variance tests and return corresponding p-values
-        pvalue_current_gen = pChi2(varLimit, PI_current_gen)
-        pvalue_preceding_gen = pChi2(varLimit, PI_preceding_gen)
-        # return TRUE, i.e. stop cmaes exectuion, if p-value is below specified significance level alpha
-        return (pvalue_current_gen <= alpha && pvalue_preceding_gen <= alpha)
-      }
-      else{
-        return(FALSE)
-      }
-    }
-  ))
-}
-
-#' @export
-pChi2 <- function (varLimit, PI) {
-  # Determine degrees of freedom
-  N = length(PI)-1
-  # calculate test statistic
-  Chi = (var(PI)*N)/varLimit
-  # get p-value of corresponding chi2 test
-  p = pchisq(Chi, N, lower.tail = TRUE)
-  return (p)
-}
-
