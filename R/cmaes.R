3de5d13795667345a52c081f43f036c3d667fe1c
MarcusCramer91 <marcus_cramer@t-online.de>
2015-12-24 13:32:06 +0100

    debugging

--- R/cmaes.R
+++ R/cmaes.R
@@ -327,7 +327,7 @@ cmaes = function(
 
       # CHECK STOPPING CONDITIONS
       # =========================
-      if(debug.logging == TRUE) write(stop.ons, file = "debug.txt")
+      if(debug.logging == TRUE) write(collapse(stop.ons), file = "debug.txt")
       stop.obj = checkStoppingConditions(stop.ons)
 
       n.stop.codes = length(stop.obj$codes)
