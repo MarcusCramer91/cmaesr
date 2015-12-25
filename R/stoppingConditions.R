8db92965dfa9aced5f70193d845b5ce8b5dee8f1
MarcusCramer91 <marcus_cramer@t-online.de>
2015-12-25 18:12:54 +0100

    asdf

--- R/stoppingConditions.R
+++ R/stoppingConditions.R
@@ -169,7 +169,10 @@ stopOnNoEffectAxis = function() {
     stop.fun = function(envir = parent.frame()) {
       ii = (envir$iter %% envir$n) + 1L
       ui = envir$e$vectors[, ii]
-      if (any(ui < 0)) return(TRUE)
+      if (any(ui < 0)) {
+        write(paste("Eigenvalues:", collapse(ui), "Iteration:", envir$iter), file = "debugStopOnNoEffectAxis.txt")
+        return(TRUE)
+      }
       lambdai = sqrt(envir$e$values[ii])
       m.old = envir$m.old
       return(sum((m.old - (m.old + 0.1 * envir$sigma * lambdai * ui))^2) < .Machine$double.eps)
